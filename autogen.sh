6dab4b79d66fa5ad5827b53d930f6508380b4f4a
Michael Krelin <hacker@klever.net>
2009-04-25 22:38:04 +0200

    autogen: mkdir aclocal.d for older libtoolize
    Signed-off-by: Michael Krelin <hacker@klever.net>

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,8 @@ tool_libtoolize="$(type -P glibtoolize || type -P libtoolize)"
 if test -z "$tool_libtoolize" ; then
  echo "Failed to find libtoolize." ; exit 1;
 fi
-test -d aux.d || mkdir aux.d \
+   (test -d aux.d || mkdir aux.d) \
+&& (test -d aclocal.d || mkdir aclocal.d) \
 && "$tool_libtoolize" -f \
 && aclocal -I aclocal.d \
 && autoheader \
