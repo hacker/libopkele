40bc4d390e016cbb64ea25be177daa92c94a8285
Michael Krelin <hacker@klever.net>
2009-04-25 22:11:02 +0200

    autogen: try to ensure aux.d existence
    Signed-off-by: Michael Krelin <hacker@klever.net>

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,8 @@ tool_libtoolize="$(type -P glibtoolize || type -P libtoolize)"
 if test -z "$tool_libtoolize" ; then
  echo "Failed to find libtoolize." ; exit 1;
 fi
-   "$tool_libtoolize" -f \
+test -d aux.d || mkdir aux.d \
+&& "$tool_libtoolize" -f \
 && aclocal -I aclocal.d \
 && autoheader \
 && automake -a \
