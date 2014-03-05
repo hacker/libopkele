00cf4285fb0539873754f23a654b9cd13ae29af4
Michael Krelin <hacker@klever.net>
2014-03-05 22:13:36 +0100

    remove post-increment implementation as it is awfully invalid
    as per github issue #5

--- include/opkele/iterator.h
+++ include/opkele/iterator.h
@@ -159,11 +159,6 @@ namespace opkele {
 			if(!found) empty=true;
 			return *this;
 		    }
-		    basic_filterator<IT> operator++(int) {
-			basic_filterator<IT> rv(*this);
-			++(*this);
-			return rv;
-		    }
 
 		    void prepare() {
 			bool found = false;
